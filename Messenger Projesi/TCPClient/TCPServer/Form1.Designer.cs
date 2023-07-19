c476d15aedff5e3157c7781feb18dc8b5b8aa861
FIRAT KAAN BİTMEZ <74864221+firatkaanbitmez@users.noreply.github.com>
2023-07-20 02:51:11 +0300

    re

---
+++ Messenger Projesi/TCPClient/TCPServer/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstClientIP = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(73, 16);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(453, 23);
+            this.txtIP.TabIndex = 0;
+            this.txtIP.Text = "127.0.0.1:9000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(451, 342);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(73, 45);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(453, 253);
+            this.txtInfo.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(73, 304);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(453, 23);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(370, 342);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lstClientIP
+            // 
+            this.lstClientIP.FormattingEnabled = true;
+            this.lstClientIP.ItemHeight = 15;
+            this.lstClientIP.Location = new System.Drawing.Point(532, 45);
+            this.lstClientIP.Name = "lstClientIP";
+            this.lstClientIP.Size = new System.Drawing.Size(180, 319);
+            this.lstClientIP.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Client IP:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 395);
+            this.Controls.Add(this.lstClientIP);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtIP);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtIP;
+        private Label label1;
+        private Button btnStart;
+        private TextBox txtInfo;
+        private TextBox txtMessage;
+        private Label label2;
+        private Button btnSend;
+        private ListBox lstClientIP;
+        private Label label3;
+    }
+}
